590c081ef63139af2b0c4c957de883ab0f904297
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-21 22:59:51 -0400

    added comments

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,9 @@ use proc_macro::{quote, TokenStream};
 pub fn equation(input: TokenStream) -> TokenStream {
     let mut equation = input.to_string();
     let mut equation_offset = 0;
-    let mut last_char = equation.chars().nth(0).unwrap();
+    let mut last_char = equation.clone().chars().nth(0).unwrap();
     let mut variables = (vec![], 0);
+
     for (i, c) in equation.clone().chars().into_iter().enumerate() {
         // if a variable is next to a number add a * inbetween the number and the variable
         if c.is_alphabetic()
@@ -17,22 +18,30 @@ pub fn equation(input: TokenStream) -> TokenStream {
             equation.insert(i + equation_offset, '*');
             equation_offset += 1;
         }
+        // if a variable is not in the variables variable then add it
         if c.is_alphabetic() && !variables.0.contains(&c) {
             variables.0.push(c);
             variables.1 += 1;
         }
         last_char = c;
     }
-    println!("Variables Amount: {variables}", variables = variables.1);
-    let var_string: TokenStream = format!("{:?} ", variables.0)
+
+    // proc macro stuff
+    let equation_clone = equation.clone();
+    let equation_split = equation_clone.split(";").collect::<Vec<&str>>();
+    let var_string_prec = format!("{:?} ", variables.0)
         .replace("[", "")
         .replace("]", "")
         .replace(",", "")
-        .replace("'", "")
-        .replace(" ", ": i32, ")
-        .parse()
-        .unwrap();
-    println!("{}", format!("|{var_string}| {{ return ({equation}); }}"));
+        .replace("'", "");
+    let var_string;
+    let equation_split_len = equation_split.len().clone();
+    // if there is a "; <type>" add the type to the variables
+    if equation_split_len != 1 {
+        var_string = var_string_prec.replace(" ", format!(": {},", equation_split[1]).as_str())
+    } else {
+        var_string = var_string_prec.replace(" ", ": i32,");
+    }
     return format!("|{var_string}| {{ return ({equation}); }}")
         .parse()
         .unwrap();
