cb5a2180fbe72081634691931f0b4918af09ece5
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-30 17:49:39 -0400

    Read me + Description

---
+++ README.md
@@ -0,0 +1,6 @@
+# This library creates a function from a math equation
+## Example
+```rs
+let x = equation!(10 * x + 3);
+assert_eq!(33, x(3));
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! # This library creates a function from a math equation
+//!
+//! ## Example
+//! ```rs
+//! let x = equation!(10 * x + 3);
+//! assert_eq!(33, x(3));
+//! ```
+
 #![feature(proc_macro_quote)]
 use proc_macro::TokenStream;
 
